e21fd1b0fffa328200e4f6a3ad40572874db2de9
Kacper <krol.kacper.pg2@gmail.com>
2024-03-09 17:17:08 +0100

    Add project files.

---
+++ dotnetmastery8net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetmastery8net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
